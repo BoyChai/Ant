a76093701995be6ee09e8b1a6e58659b6ea5ed04
BoyChai <1972567225@qq.com>
2023-05-26 23:31:37 +0800

    Add Verification Rules

--- rules.go
+++ rules.go
@@ -6,7 +6,14 @@ import (
 	"regexp"
 )
 
-var rules = rulesMap{}
+var rules = rulesMap{
+	// 表单相关
+	"email": `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`, // 邮箱
+	// 网络相关
+	"network_transport_protocol_common": `(?i)^(UDP|TCP)$`,                            // 常用的网络传输协议 (TCP|UDP)
+	"addr_ipv4":                         `^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$`,            // ipv4地址
+	"addr_ipv6":                         `^(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$`, // 标准的ipv6地址
+}
 
 type rulesMap map[string]string
 
