55fabca04e1780c22f1518304aa6ca0715e19ddc
BoyChai <1972567225@qq.com>
2023-05-26 23:39:30 +0800

    Add Port Verification Rule

--- rules.go
+++ rules.go
@@ -10,9 +10,11 @@ var rules = rulesMap{
 	// 表单相关
 	"email": `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`, // 邮箱
 	// 网络相关
-	"network_transport_protocol_common": `(?i)^(UDP|TCP)$`,                            // 常用的网络传输协议 (TCP|UDP)
-	"addr_ipv4":                         `^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$`,            // ipv4地址
-	"addr_ipv6":                         `^(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$`, // 标准的ipv6地址
+	"network_transport_protocol_common": `(?i)^(UDP|TCP)$`,                                           // 常用的网络传输协议 (TCP|UDP)
+	"port":                              `^([1-9]|[1-9]\d{1,3}|[1-5]\d{4}|[6][0-5][0-5][0-3][0-5])$`, // 端口 (1-65535)
+	"addr_ipv4":                         `^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$`,                           // ipv4地址
+	"addr_ipv6":                         `^(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$`,                // 标准的ipv6地址
+
 }
 
 type rulesMap map[string]string
