f8ae203deb67e6472bdc97482e8a52cb53a02f71
BoyChai <1972567225@qq.com>
2023-05-27 10:50:50 +0800

    Update Struct Judgment Adaptation Slice

--- validator.go
+++ validator.go
@@ -35,21 +35,31 @@ func (v *Validator) Struct(s interface{}, filePath ...string) Error {
 			}
 			continue
 		}
-		// 检查是否需要数据检查
-		if ruleName != "" {
-			// 拿到正则检查对象
-			rule, e := rules.getRule(ruleName)
-			if e != nil {
-				err = append(err, fmt.Sprintf(formatFieldName(filePath, fieldName)+": "+ruleName+" "+e.Error()))
-				continue
+
+		// 切片类型处理
+		if fieldType == reflect.Slice {
+			fmt.Println(fieldType)
+			fmt.Println(fieldName)
+			sliceValue := value.FieldByName(fieldName)
+			fmt.Println(sliceValue)
+			for j := 0; j < sliceValue.Len(); j++ {
+				sliceFieldName := fieldName + "[" + fmt.Sprint(j) + "]"
+				elemValue := sliceValue.Index(j)
+				fmt.Println(elemValue)
+				if elemValue.Kind() == reflect.Struct {
+					e := v.Struct(elemValue.Interface(), formatFieldName(filePath, sliceFieldName))
+					for i := 0; i < len(e.Data); i++ {
+						err = append(err, e.Data[i].Value)
+					}
+				}
+				// 检查是否需要数据检查
+				err = append(err, checkValue(ruleName, value, filePath, fieldName, sliceFieldName)...)
 			}
-			// 使用正则对象进行检查
-			isMatch := rule.MatchString(fmt.Sprint(value.FieldByName(fieldName).Interface()))
-			if !isMatch {
-				err = append(err, fmt.Sprintf(formatFieldName(filePath, fieldName)+": Check failed"))
-				continue
-			}
+			continue
 		}
+
+		// 检查是否需要数据检查
+		err = append(err, checkValue(ruleName, value, filePath, fieldName)...)
 	}
 	return formatError(err)
 }
@@ -60,6 +70,7 @@ func formatFieldName(parentPath []string, fieldName string) string {
 	return path
 }
 
+// 格式化错误输出
 func formatError(data []string) Error {
 	if len(data) <= 0 {
 		return Error{}
@@ -70,4 +81,31 @@ func formatError(data []string) Error {
 	}
 	e := Error{Is: true, Data: err}
 	return e
+}
+
+func checkValue(ruleName string, value reflect.Value, filePath []string, fieldName string, sliceFieldName ...string) []string {
+	var err []string
+	if ruleName != "" {
+		// 拿到正则检查对象
+		rule, e := rules.getRule(ruleName)
+		if e != nil {
+			if len(sliceFieldName) == 0 {
+				err = append(err, fmt.Sprintf(formatFieldName(filePath, fieldName)+": "+ruleName+" "+e.Error()))
+			} else {
+				err = append(err, fmt.Sprintf(formatFieldName(filePath, sliceFieldName[0])+": "+ruleName+" "+e.Error()))
+			}
+			return err
+		}
+		// 使用正则对象进行检查
+		isMatch := rule.MatchString(fmt.Sprint(value.FieldByName(fieldName).Interface()))
+		if !isMatch {
+			if len(sliceFieldName) == 0 {
+				err = append(err, fmt.Sprintf(formatFieldName(filePath, fieldName)+": Check failed"))
+			} else {
+				err = append(err, fmt.Sprintf(formatFieldName(filePath, sliceFieldName[0])+": Check failed"))
+			}
+			return err
+		}
+	}
+	return err
 }
